c3f2c1f199c15c9b8ddcf8290ec474eb70c6d117
DarkL0rdd <vadimka1235931@gmail.com>
2022-06-06 22:51:24 +0300

    code structuring and in user.service.ts

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -4,45 +4,36 @@ import { removeToken } from "../services/jwt.service";
 
 const userSequelize = sequelize.getRepository(User);
 
-export const createUser = async (
-  reqFirstName: string,
-  reqSecondName: string,
-  reqEmail: string,
-  reqPassword: string
-) => {
-  let user: User | undefined = undefined;
+export const createUser = async (reqFirstName: string, reqSecondName: string, reqEmail: string, reqPassword: string) => {
   try {
-    return (user = await userSequelize.create({
+    return await userSequelize.create({
       first_name: reqFirstName,
       second_name: reqSecondName,
       email: reqEmail,
       password: reqPassword,
       refresh_token: "",
-    }));
+    });
   } catch (err) {
     console.log(err);
-    return (user = undefined);
   }
 };
 
 export const logout = async (refreshToken: string) => {
   try {
-    await removeToken(refreshToken);
+    return await removeToken(refreshToken);
   } catch (err) {
     console.log(err);
   }
 };
 
 export const getAllUsers = async () => {
-  let users: User[] | undefined = undefined;
   try {
-    return (users = await userSequelize.findAll({
+    return await userSequelize.findAll({
       attributes: {
         exclude: ["password", "refresh_token", "createdAt", "updatedAt"],
       },
-    }));
+    });
   } catch (err) {
     console.log(err);
-    return (users = undefined);
   }
 };
