ff4098260aa1960ba8c5c074e90c626bf7d83eb6
DarkL0rdd <vadimka1235931@gmail.com>
2022-06-21 22:38:04 +0300

    add throw error in email service

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,32 +1,28 @@
 import { sequelize } from "../sequelize";
 import { User } from "../models/user.model";
+import { CustomError } from "./error.service";
 
 const userSequelize = sequelize.getRepository(User);
 
 export const findUserEmail = async (reqEmail: string) => {
-  try {
-    const emailFind = await userSequelize.findOne({
-      where: { email: reqEmail },
-    });
-    if (emailFind) {
-      const email = emailFind.email;
-      const id = emailFind.id;
-      return { email, id };
-    }
-  } catch (err) {
-    console.log(err);
+  const emailFind = await userSequelize.findOne({
+    where: { email: reqEmail },
+  });
+  if (emailFind) {
+    const email = emailFind.email;
+    const id = emailFind.id;
+    return { email, id };
   }
+  throw new CustomError(401, `User with email ${reqEmail} is not found.`);
 };
 
 export const compareUserEmail = async (reqNewEmail: string) => {
-  try {
-    const userEmail = await findUserEmail(reqNewEmail);
-    if (userEmail?.email === reqNewEmail) {
-      return true;
-    } else {
-      return false;
-    }
-  } catch (err) {
-    console.log(err);
+  const userEmail = await userSequelize.findOne({
+    where: { email: reqNewEmail },
+  });
+  if (userEmail?.email === reqNewEmail) {
+    throw new CustomError(409, `User with email ${reqNewEmail} already exist.`);
+  } else {
+    return userEmail;
   }
 };
