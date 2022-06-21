fa6dbcd2b7715ab90b96c1ffee7fe2239eff2f4e
DarkL0rdd <vadimka1235931@gmail.com>
2022-06-21 22:38:31 +0300

    add throw error in jwt service

--- src/services/jwt.service.ts
+++ src/services/jwt.service.ts
@@ -1,67 +1,42 @@
 import { sequelize } from "../sequelize";
 import { User } from "../models/user.model";
 import jwt from "jsonwebtoken";
+import { CustomError } from "./error.service";
 
 const userSequelize = sequelize.getRepository(User);
 
 export const generateAccessToken = async (reqEmail: string, timeAccesss: string) => {
-  try {
-    const accessToken = jwt.sign({ reqEmail }, `${process.env.ACCESS_SECRET_KEY}`, { expiresIn: timeAccesss });
-    return accessToken;
-  } catch (err) {
-    console.log(err);
-  }
+  const accessToken = jwt.sign({ reqEmail }, `${process.env.ACCESS_SECRET_KEY}`, { expiresIn: timeAccesss });
+  if (!accessToken) throw new CustomError(500, "Error generate access token.");
+  return accessToken;
 };
 
 export const generateRefreshToken = async (reqEmail: string, timeRefresh: string) => {
-  try {
-    const refreshToken = jwt.sign({ reqEmail }, `${process.env.REFRESH_SECRET_KEY}`, { expiresIn: timeRefresh });
-    return refreshToken;
-  } catch (err) {
-    console.log(err);
-  }
+  const refreshToken = jwt.sign({ reqEmail }, `${process.env.REFRESH_SECRET_KEY}`, { expiresIn: timeRefresh });
+  if (!refreshToken) throw new CustomError(500, "Error generate refresh token.");
+  return refreshToken;
 };
 
 export const saveToken = async (reqUserId: number, refreshToken: string) => {
-  try {
-    const userId = await userSequelize.findOne({ where: { id: reqUserId } });
-    if (userId) {
-      userId.refresh_token = refreshToken;
-      return userId.save();
-    }
-  } catch (err) {
-    console.log(err);
-  }
+  const userId = await userSequelize.findOne({ where: { id: reqUserId } });
+  if (!userId) throw new CustomError(404, "User not found.");
+  userId.refresh_token = refreshToken;
+  return userId.save();
 };
 
-export const removeToken = async (refreshToken: string) => {
-  try {
-    return await userSequelize.update(
-      { refresh_token: undefined },
-      {
-        where: { refresh_token: refreshToken },
-      }
-    );
-  } catch (err) {
-    console.log(err);
-  }
+export const removeRefreshToken = async (refreshTokenFromDb: string) => {
+  return await userSequelize.update(
+    { refresh_token: undefined },
+    {
+      where: { refresh_token: refreshTokenFromDb },
+    }
+  );
 };
 
 export const verifyRefreshToken = async (refreshToken: string) => {
   //authenticateRefreshToken
   try {
     return jwt.verify(refreshToken, `${process.env.REFRESH_SECRET_KEY}`);
-    //let payloadUser;
-    //payloadUser = jwt.verify(refreshToken, `${process.env.REFRESH_SECRET_KEY}`);
-    /*jwt.verify(refreshToken, `${process.env.REFRESH_SECRET_KEY}`, (err, payload) => {
-      if (err) {
-        console.log(err);
-        //payloadUser = false;
-      } else {
-        payloadUser = payload;
-      }
-    });*/
-    //return payloadUser;
   } catch (err) {
     console.log(err);
   }
@@ -71,17 +46,6 @@ export const verifyAccessToken = async (accessToken: string) => {
   //authenticateAccessToken
   try {
     return jwt.verify(accessToken, `${process.env.ACCESS_SECRET_KEY}`);
-    //let payloadUser;
-    //payloadUser = jwt.verify(accessToken, `${process.env.ACCESS_SECRET_KEY}`);
-    /*jwt.verify(accessToken, `${process.env.ACCESS_SECRET_KEY}`, (err, payload) => {
-      if (err) {
-        console.log(err);
-        //payloadUser = false;
-      } else {
-        payloadUser = payload;
-      }
-    });*/
-    //return payloadUser;
   } catch (err) {
     console.log(err);
   }
