5ad7f4a7f4870b203a4f7f92864844345995a290
DarkL0rdd <vadimka1235931@gmail.com>
2022-06-21 22:14:25 +0300

    create file and add enums

---
+++ src/services/all.enums.ts
@@ -0,0 +1,44 @@
+export const enum HashRound {
+  OneRound = 1,
+  FiveRound = 5,
+  EightRound = 8,
+}
+
+export const enum UserRole {
+  Admin = "Admin",
+  Manager = "Manager",
+  Player = "Player",
+}
+
+export const enum UserStatus {
+  Active = "Active",
+  Blocked = "Blocked",
+}
+
+export enum PlayerRequest {
+  BecomeManager = "Change role to Manager",
+  AddToTeam = "Add to the team",
+  ExitTeam = "Exit from team",
+  ChangeTeam = "Change team",
+}
+
+export const enum RequestStatus {
+  Approved = "Approved",
+  Declined = "Declined",
+  Pending = "Pending",
+}
+
+export const enum Panel {
+  AdminPanel = "admin-panel",
+  ManagerPanel = "manager-panel",
+  PlayerPanel = "player-panel",
+}
+
+export const enum List {
+  AdminList = "list-admins",
+  ManagerList = "list-managers",
+  PlayerList = "list-players",
+  TeamList = "list-teams",
+  MyList = "my-list-requests",
+  AllList = "list-requests",
+}
