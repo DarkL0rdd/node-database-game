fe0502b128c6caf5c5489c378bdb98a93aa73dc8
DarkL0rdd <vadimka1235931@gmail.com>
2022-06-23 22:32:29 +0300

    delete and add new routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -10,17 +10,17 @@ import {
   updateInfoUserProfile,
   banUser,
   unbanUser,
-  createNewRequest,
   showInfoAllUsersByRole,
   showInfoUserByRoleAndId,
 } from "../controllers/user.controllers";
 import { authenticateAccessToken } from "../middleware/authorization.JWT";
 import { checkRole } from "../middleware/check.roles";
-import { UserRole } from "../services/user.service";
+import { validatePanel } from "../middleware/validate.list";
+import { UserRole } from "../services/all.enums";
 
 export const userRouter: Application = router();
 
-//!User
+//User
 userRouter.post("/register", registrationNewUser);
 userRouter.post("/login", loginUser);
 userRouter.all("/refresh", generateNewTokens); //? куди далі перейти ?
@@ -40,54 +40,26 @@ userRouter.post(
   updateInfoUserProfile
 );
 
-//!List of params: admin-panel, manager-panel, player-panel | list-admins, list-managers, list-players
+//!List of params: admin-panel, manager-panel, player-panel | list-admins, list-managers, list-players, list-teams, list-requests, my-list-requests
 
+//All: Show list of admins/managers/players
 userRouter.get(
-  //!All: Show list of admins or managers or players
-  "/profile/:panel/:list/",
+  "/profile/:panel/:list",
   authenticateAccessToken,
   checkRole(UserRole.Admin, UserRole.Manager, UserRole.Player),
+  validatePanel(),
   showInfoAllUsersByRole
 );
-
+//All: Show one admin/manager/player by ID
 userRouter.get(
-  //!All: Show one admin or manager or player by ID
   "/profile/:panel/:list/:id",
   authenticateAccessToken,
   checkRole(UserRole.Admin, UserRole.Manager, UserRole.Player),
+  validatePanel(),
   showInfoUserByRoleAndId
 );
 
-userRouter.post("/profile/admin-panel/:list/:id/ban", authenticateAccessToken, checkRole(UserRole.Admin), banUser); //!Admin: Ban manager or player by ID
-userRouter.post("/profile/admin-panel/:list/:id/unban", authenticateAccessToken, checkRole(UserRole.Admin), unbanUser); //!Admin: Unban manager or player by ID
-
-userRouter.post(
-  //!Manager, Player: Create new request
-  "/profile/:panel/my-list-requests/new-request",
-  authenticateAccessToken,
-  checkRole(UserRole.Manager, UserRole.Player),
-  createNewRequest
-);
-
-//!Admin, Manager: Show list of requests(managers or players)
-//userRouter.get("/profile/:panel/list-requests", authenticateAccessToken, checkRole(UserRole.Admin, UserRole.Manager, UserRole.Player), showAllRequests);
-//!Admin, Manager: Show one requests by ID(manager or player)
-//userRouter.get("/profile/:panel/list-requests/:id", authenticateAccessToken, checkRole(UserRole.Admin, UserRole.Manager, UserRole.Player), showRequestById);
-
-//!Manager, Player: Show list of OWN requests
-//userRouter.get("/profile/:panel/my-list-requests", authenticateAccessToken, checkRole(UserRole.Admin, UserRole.Manager, UserRole.Player), showAllRequests);
-//!Manager, Player: Show OWN request by ID
-//userRouter.get("/profile/:panel/my-list-requests/:id", authenticateAccessToken, checkRole(UserRole.Admin, UserRole.Manager, UserRole.Player), showRequestById);
-
-//!Admin, Manager: Answer to request
-//!Admin can ban/unban manager or player. Manager CANNOT ban/unban player
-//userRouter.post("/profile/:panel/list-requests/:id", authenticateAccessToken, checkRole(UserRole.Admin, UserRole.Manager) answerToRequest);
-
-//userRouter.get("/profile/:panel/list-teams", authenticateAccessToken, checkRole(UserRole.Admin, UserRole.Manager, UserRole.Player), showInfoAllUsers);//!All: Show list of teams
-//userRouter.get("/profile/:panel/list-teams/:id", authenticateAccessToken, checkRole(UserRole.Admin, UserRole.Manager, UserRole.Player), showInfoByIdUser);//!All: Show team information by ID
-
-//!Admin, Manager: Delete player from team
-//userRouter.post("/profile/:panel/:list/:id/delete-from-team", authenticateAccessToken, checkRole(UserRole.Admin), deletePlayerFromTeam);
-
-//!(Manager???), Player: Cancel request
-//userRouter.post("/profile/:panel/my-list-requests/:id", authenticateAccessToken, checkRole(UserRole.Admin), cancelRequest);
+//Admin: Ban manager/player by ID
+userRouter.post("/profile/admin-panel/:list/:id/ban", authenticateAccessToken, checkRole(UserRole.Admin), banUser);
+//Admin: Unban manager/player by ID
+userRouter.post("/profile/admin-panel/:list/:id/unban", authenticateAccessToken, checkRole(UserRole.Admin), unbanUser);
