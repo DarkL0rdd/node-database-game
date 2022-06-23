c80b96a9cb1955c880165d8cc2d50d5251be5b1d
DarkL0rdd <vadimka1235931@gmail.com>
2022-06-23 22:42:55 +0300

    add custom errors to the validate-list

--- src/middleware/check.roles.ts
+++ src/middleware/check.roles.ts
@@ -2,22 +2,28 @@ import { sequelize } from "../sequelize";
 import { Request, Response, NextFunction } from "express";
 import { User } from "../models/user.model";
 import { Role } from "../models/role.model";
+import { CustomError } from "../services/error.service";
 
 const userSequelize = sequelize.getRepository(User);
 const roleSequelize = sequelize.getRepository(Role);
 
 export const checkRole = (...roles: string[]) => {
   const funct = async (req: Request, res: Response, next: NextFunction) => {
-    const user = await userSequelize.findOne({
-      where: { email: req.user.reqEmail },
-      attributes: ["id", "role_id", "email"],
-      include: [{ model: roleSequelize, attributes: ["role_name"] }],
-    });
-    if (!user) return res.status(404).json({ Message: "User is not found." });
-    if (!user || !roles.includes(user.role.role_name)) {
-      return res.status(403).send({ error: { status: 403, message: "Access denied." } });
+    try {
+      const user = await userSequelize.findOne({
+        where: { email: req.user.reqEmail },
+        attributes: ["id", "role_id", "email"],
+        include: [{ model: roleSequelize, attributes: ["role_name"] }],
+      });
+      if (!user) throw new CustomError(404, "User is not found.");
+      if (!roles.includes(user.role.role_name)) throw new CustomError(403, "You don't have permission to access this page.");
+      res.locals.userRole = user.role.role_name;
+      console.log("checkRole");
+      next();
+    } catch (err) {
+      console.log(err);
+      res.status(err.status).json({ Message: err.message });
     }
-    next();
   };
   return funct;
 };
