da1cc602926646377aa03342305b5db8119222a7
DarkL0rdd <vadimka1235931@gmail.com>
2022-07-14 21:51:18 +0300

    delete compareUserEmail function from email.service.ts

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -7,6 +7,7 @@ const userSequelize = sequelize.getRepository(User);
 export const findUserEmail = async (reqEmail: string) => {
   const emailFind = await userSequelize.findOne({
     where: { email: reqEmail },
+    attributes: ["email", "id"],
   });
   if (emailFind) {
     const email = emailFind.email;
@@ -14,15 +15,4 @@ export const findUserEmail = async (reqEmail: string) => {
     return { email, id };
   }
   throw new CustomError(401, `User with email ${reqEmail} is not found.`);
-};
-
-export const compareUserEmail = async (reqNewEmail: string) => {
-  const userEmail = await userSequelize.findOne({
-    where: { email: reqNewEmail },
-  });
-  if (userEmail?.email === reqNewEmail) {
-    throw new CustomError(409, `User with email ${reqNewEmail} already exist.`);
-  } else {
-    return userEmail;
-  }
 };
