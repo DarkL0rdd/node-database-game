4e0620ea94f0588aff49f0ad96eda0ee0abe9b1b
DarkL0rdd <vadimka1235931@gmail.com>
2022-07-15 23:19:37 +0300

    change logic deletePlayerFromTeamById function in team.service.ts

--- src/services/team.service.ts
+++ src/services/team.service.ts
@@ -1,20 +1,16 @@
 import { sequelize } from "../sequelize";
 import { User } from "../models/user.model";
-import { Role } from "../models/role.model";
-import { UserRequest } from "../models/userrequest.model";
 import { Team } from "../models/team.model";
 import { CustomError } from "./error.service";
 
 const userSequelize = sequelize.getRepository(User);
-const roleSequelize = sequelize.getRepository(Role);
 const teamSequelize = sequelize.getRepository(Team);
-const requestSequelize = sequelize.getRepository(UserRequest);
 
 export const getInfoAllTeams = async () => {
   const teams = await teamSequelize.findAll({
     attributes: ["id", "team_name"],
   });
-  if (!teams) throw new CustomError(500, "Teams not found.");
+  if (teams.length === 0) throw new CustomError(500, "Teams not found.");
   return teams;
 };
 
@@ -28,15 +24,13 @@ export const getInfoTeamById = async (teamId: string) => {
   return team;
 };
 
-export const deletePlayerFromTeamById = async (userId: string, teamId: string) => {
-  const user = await userSequelize.findOne({
-    where: { id: userId },
-    attributes: ["id", "team_id"],
-    include: [{ model: teamSequelize, where: { id: teamId } }],
-  });
-
-  console.log(user?.toJSON());
-
-  if (!user) throw new CustomError(404, "User not found.");
-  return await user.update({ team_id: 0 }); //! undefined
+export const deletePlayerFromTeamById = async (userId: string) => {
+  const affectedRow = await userSequelize.update(
+    { team_id: null },
+    {
+      where: { id: userId },
+    }
+  );
+  if (affectedRow[0] === 1) return affectedRow;
+  throw new CustomError(500, "Error delete player from team.");
 };
