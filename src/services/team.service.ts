382f445ff1d562536de842e5eb90cea463dcfe3c
DarkL0rdd <vadimka1235931@gmail.com>
2022-07-25 17:06:00 +0300

    create addPlayerInTeam function and add description for functions in team.service.ts

--- src/services/team.service.ts
+++ src/services/team.service.ts
@@ -6,6 +6,11 @@ import { CustomError } from "./error.service";
 const userSequelize = sequelize.getRepository(User);
 const teamSequelize = sequelize.getRepository(Team);
 
+/**
+ * Find all teams in the database.
+ * @returns Promise array of objects with `id`, `team_name`.
+ * @throws `CustomError` if teams not found in the database(`teams.length` equal to zero(`0`)).
+ */
 export const getInfoAllTeams = async () => {
   const teams = await teamSequelize.findAll({
     attributes: ["id", "team_name"],
@@ -14,6 +19,12 @@ export const getInfoAllTeams = async () => {
   return teams;
 };
 
+/**
+ * Find one team in the database.
+ * @param teamId Team ID.
+ * @returns Promise object with team: `id`, `team_name`. And users that are in the team: `id`, `first_name`, `second_name`, `email`, `team_id`.
+ * @throws `CustomError` if `teamId` not found in the database.
+ */
 export const getInfoTeamById = async (teamId: string) => {
   const team = await teamSequelize.findOne({
     where: { id: teamId },
@@ -24,6 +35,36 @@ export const getInfoTeamById = async (teamId: string) => {
   return team;
 };
 
+/**
+ * Adding a user to the team.
+ * @param teamName Team name.
+ * @param userId User id.
+ * @returns Promise an array of one element. The first element is the number of affected rows.
+ * @throws `CustomError` if `teamName` not found in the database, or `affectedRow` is not equal to one(`1`).
+ */
+export const addPlayerInTeam = async (teamName: string, userId: string) => {
+  const team = await teamSequelize.findOne({
+    where: {
+      team_name: teamName,
+    },
+  });
+  if (!team) throw new CustomError(404, "Team not found.");
+  const affectedRow = await userSequelize.update(
+    { team_id: team.id },
+    {
+      where: { id: userId },
+    }
+  );
+  if (affectedRow[0] === 1) return affectedRow;
+  throw new CustomError(500, "Error adding a player to the team.");
+};
+
+/**
+ * Deleting a user from the team.
+ * @param userId User id.
+ * @returns Promise an array of one element. The first element is the number of affected rows.
+ * @throws `CustomError` if `userId` not found in the database, or `affectedRow` is not equal to one(`1`).
+ */
 export const deletePlayerFromTeamById = async (userId: string) => {
   const affectedRow = await userSequelize.update(
     { team_id: null },
@@ -32,5 +73,5 @@ export const deletePlayerFromTeamById = async (userId: string) => {
     }
   );
   if (affectedRow[0] === 1) return affectedRow;
-  throw new CustomError(500, "Error delete player from team.");
+  throw new CustomError(500, "Error removing a player from the team.");
 };
