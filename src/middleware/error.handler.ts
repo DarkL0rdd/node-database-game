0c9a7564ba0e0d94798a4add05a2f815fbd4207d
DarkL0rdd <vadimka1235931@gmail.com>
2022-07-25 16:54:00 +0300

    add function description for middleware errorHandler

--- src/middleware/error.handler.ts
+++ src/middleware/error.handler.ts
@@ -1,6 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import { CustomError } from "../services/error.service";
 
+/**
+ * Global error-handler middleware.
+ * @param err Error object.
+ * @param req Express request object.
+ * @param res Express response object.
+ * @param next Express next function.
+ */
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
   console.log("Error-handler: ", err);
   let customError = err;
