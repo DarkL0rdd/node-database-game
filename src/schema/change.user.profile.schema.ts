a2d1e8ab04be39e0defe5acbd7bcb311653a2be2
DarkL0rdd <vadimka1235931@gmail.com>
2022-07-30 22:01:45 +0300

    change validation-schema

--- src/schema/change.user.profile.schema.ts
+++ src/schema/change.user.profile.schema.ts
@@ -7,27 +7,27 @@ const userSequelize = sequelize.getRepository(User);
 
 export const changeUserProfileSchema = [
   //first_name
-  body("first_name").optional({ checkFalsy: true }).not().isEmpty().withMessage("First name is required."),
-  body("first_name").optional({ checkFalsy: true }).exists({ checkFalsy: true }).withMessage(`First name is required.`),
-  body("first_name").optional({ checkFalsy: true }).isString().withMessage("First name type error."),
+  body("first_name").optional({ checkFalsy: false }).not().isEmpty().withMessage("First name is required."),
+  body("first_name").optional({ checkFalsy: false }).isString().withMessage("First name type error."),
   body("first_name")
     .optional({ checkFalsy: true })
     .isLength({ min: 1, max: 50 })
     .withMessage("First name cannot be shorter than 1 character and longer than 50 characters."),
 
   //second_name
-  body("second_name").optional({ checkFalsy: true }).exists({ checkFalsy: true }).withMessage("Second name is required."),
-  body("second_name").optional({ checkFalsy: true }).isString().withMessage("Second name type error."),
+  body("second_name").optional({ checkFalsy: false }).not().isEmpty().withMessage("Second name is required."),
+  body("second_name").optional({ checkFalsy: false }).isString().withMessage("Second name type error."),
   body("second_name")
-    .optional({ checkFalsy: true })
+    .optional({ checkFalsy: false })
     .isLength({ min: 1, max: 50 })
     .withMessage("Second name cannot be shorter than 1 character and longer than 50 characters."),
 
   //email
-  body("email").optional({ checkFalsy: true }).isEmail().normalizeEmail().withMessage("Email is not valid."),
-  body("email").optional({ checkFalsy: true }).isString().withMessage("Email type error."),
+  body("email").optional({ checkFalsy: false }).not().isEmpty().withMessage("Email is required."),
+  body("email").optional({ checkFalsy: false }).isString().withMessage("Email type error."),
+  body("email").optional({ checkFalsy: false }).isEmail().normalizeEmail().withMessage("Email is not valid."),
   body("email")
-    .optional({ checkFalsy: true })
+    .optional({ checkFalsy: false })
     .custom(async (value) => {
       const userEmail = await userSequelize.findOne({
         where: { email: value },
@@ -36,14 +36,15 @@ export const changeUserProfileSchema = [
       if (userEmail) throw new CustomError(409, `User with email ${value} already exist.`);
     }),
   body("email")
-    .optional({ checkFalsy: true })
+    .optional({ checkFalsy: false })
     .isLength({ min: 1, max: 254 })
     .withMessage("Email cannot be shorter than 1 character and longer than 254 characters."),
 
   //password
-  body("password").optional({ checkFalsy: true }).isString().withMessage("Password type error."),
+  body("password").optional({ checkFalsy: false }).not().isEmpty().withMessage("Password is required."),
+  body("password").optional({ checkFalsy: false }).isString().withMessage("Password type error."),
   body("password")
-    .optional({ checkFalsy: true })
+    .optional({ checkFalsy: false })
     .isLength({ min: 8, max: 100 })
     .withMessage("Password cannot be shorter than 8 characters and longer than 100 characters."),
 ];
--- src/schema/user.request.schema.ts
+++ src/schema/user.request.schema.ts
@@ -3,13 +3,23 @@ import { RequestType } from "../services/all.enums";
 
 export const userRequestSchema = [
   //request_type
-  body("request_type").isString().isIn(Object.values(RequestType)).withMessage("Request type is not valid."),
+  body("request_type").not().isEmpty().withMessage("Request type is required."),
+  body("request_type").isString().withMessage("Request type is not valid."),
+  body("request_type").isIn(Object.values(RequestType)).withMessage("Request type is not valid."),
 
   //description
-  body("description").isString(),
-  body("description").isLength({ max: 254 }).withMessage("Description can contain up to 254 characters."),
+  body("description").optional({ checkFalsy: false }).not().isEmpty().withMessage("Description is required."),
+  body("description").optional({ checkFalsy: false }).isString().withMessage("Description type error."),
+  body("description")
+    .optional({ checkFalsy: false })
+    .isLength({ max: 254 })
+    .withMessage("Description can contain up to 254 characters."),
 
   //team_name
-  body("team_name").isString(),
-  body("team_name").isLength({ max: 100 }).withMessage("Team name can contain up to 100 characters."),
+  body("team_name").optional({ checkFalsy: false }).not().isEmpty().withMessage("Team name is required."),
+  body("team_name").optional({ checkFalsy: false }).isString().withMessage("Team name type error."),
+  body("team_name")
+    .optional({ checkFalsy: false })
+    .isLength({ max: 100 })
+    .withMessage("Team name can contain up to 100 characters."),
 ];
